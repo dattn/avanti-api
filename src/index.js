a6dd1c2ce625586273809823f891d4571d08e5de
Daniel Duton <daniel@duton.lu>
2017-06-02 17:17:01 +0200

    add first controller

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Koa from 'koa';
 import Router from 'koa-router';
+import { handle } from './controller';
 
 const app = new Koa();
 const router = new Router();
 
+router.post('/token', handle('host@list'));
 app
     .use(router.routes())
     .use(router.allowedMethods());
