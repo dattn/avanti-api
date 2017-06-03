d9c02c9221c858584c7c2572d0da3d2ab16ec1c6
Daniel Duton <daniel@duton.lu>
2017-06-03 23:25:36 +0200

    add setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import Koa from 'koa';
 import Router from 'koa-router';
 import { handle } from './controller';
+import setup from 'avanti-core/dist/setup';
 
-const app = new Koa();
-const router = new Router();
+setup().then(() => {
+    const app = new Koa();
+    const router = new Router();
 
-router.get('/host/list', handle('host@list'));
-app
+    router.get('/host/list', handle('host@list'));
+    app
     .use(router.routes())
     .use(router.allowedMethods());
 
-app.listen(3000);
+    app.listen(3000);
+});
