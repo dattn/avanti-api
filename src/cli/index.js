116ee858701cdb533dbd6093fc1f0c6f77dfaa06
Daniel Duton <daniel@duton.lu>
2017-06-06 16:10:06 +0200

    use config when generating token

--- src/cli/index.js
+++ src/cli/index.js
@@ -2,27 +2,31 @@ import yargonaut from 'yargonaut';
 import yargs from 'yargs';
 import packageInfo from '../../package.json';
 import * as Token from '../token';
+import setup from 'avanti-core/dist/setup';
 
-yargonaut
-    .style('blue')
-    .helpStyle('green')
-    .errorsStyle('red.bold');
+setup().then(() => {
+    yargonaut
+        .style('blue')
+        .helpStyle('green')
+        .errorsStyle('red.bold');
 
-var options = yargs
-    .version(packageInfo.version)
+    var options = yargs
+        .version(packageInfo.version)
 
-    .command('token [ip]', 'generate token', {}, argv => {
-        Token.create(argv.ip)
-            .then(token => {
-                console.log(token);
-            });
-    })
+        .command('token [ip]', 'generate token', {}, async argv => {
+            await Token.create(argv.ip)
+                .then(token => {
+                    console.log(token);
+                });
+            process.exit();
+        })
 
-    .recommendCommands()
-    .help()
-    .argv;
+        .recommendCommands()
+        .help()
+        .argv;
 
-if (options._.length === 0) {
-    yargs.showHelp();
-    process.exit();
-}
+    if (options._.length === 0) {
+        yargs.showHelp();
+        process.exit();
+    }
+});
